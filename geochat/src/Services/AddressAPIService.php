c036ba3fb942ff3fae4ab013051cf2f5a3a7fc7b
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-12 11:48:24 +0100

    api ajoutée

---
+++ geochat/src/Services/AddressAPIService.php
@@ -0,0 +1,10 @@
+<?php
+// src/Service/AddressAPIService.php
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class AddressAPIService
+{
+    public const base_uri = 'https://api-adresse.data.gouv.fr/';
+}
