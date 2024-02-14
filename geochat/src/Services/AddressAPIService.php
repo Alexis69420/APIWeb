3e55e851978fcc8bcccdaf1a2a175c7f4be58ae1
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-14 14:25:18 +0100

    première version de la fonction faite

--- geochat/src/Services/AddressAPIService.php
+++ geochat/src/Services/AddressAPIService.php
@@ -7,4 +7,26 @@ use GuzzleHttp\Client;
 class AddressAPIService
 {
     public const BASE_URI = 'https://api-adresse.data.gouv.fr/';
+
+    public function getLngLat(string $address): ?array
+    {
+        if(strlen($address)<3){
+            return null;
+        }
+        $searchQ = self::BASE_URI . 'search/?q='.$address;
+        $client = new Client();
+        $response = $client->request('GET', $searchQ);
+        echo $response->getStatusCode(); // 200
+        echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+        echo $response->getBody();
+        //send asyncronous request
+        $promise = $client->requestAsync('GET', $searchQ)->then(function ($response) {
+            echo $response->getBody();
+        });
+        $promise->wait();
+        if($response->getBody() != null){
+            return json_decode($response->getBody(), true);
+        }
+        return null;
+    }
 }
