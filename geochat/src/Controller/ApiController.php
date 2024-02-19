2940fc9a57796d1a76c838557c368ecfcaac03f9
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-19 14:34:24 +0100

    serializer intégré

--- geochat/src/Controller/ApiController.php
+++ geochat/src/Controller/ApiController.php
@@ -7,11 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MessageRepository;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
+    #[Route('/', name: 'app_api')]
     public function index(): Response
     {
         return $this->render('api/index.html.twig', [
@@ -19,12 +20,25 @@ class ApiController extends AbstractController
         ]);
     }
 
-    #[View()]
-    #[Route('/message', name: 'app_message_index', methods: ['GET'])]
+    #[View(serializerGroups: ["message-basic"])]
+    #[Route('/messages', name: 'app_message_index', methods: ['GET'])]
     public function messages(MessageRepository $messageRepository)
     {
-        return [
-            "messages" => $messageRepository->findAll()
-        ];
+        return $messageRepository->findAll();
     }
+
+    public function serialize(SerializerInterface $serializer, MessageRepository $messageRepository): Response
+    {
+        $messages = $messageRepository->findAll();
+        $data = $serializer->serialize($messages, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            },
+            'ignored_attributes' => ['user']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
 }
